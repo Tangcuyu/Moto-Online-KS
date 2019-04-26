0ad9070afe42a7d45bd923d5862911e1fb717043
tangcuyu <joysmshr@126.com>
2019-04-26 17:07:39 +0800

    taxon

--- src/interfaces/taxon.ts
+++ src/interfaces/taxon.ts
@@ -1,4 +1,6 @@
-/* 分类单元 */
+/* 分类单元
+ * permalink： The end of the URL a user goes to, in order to see all products associated with this taxon. This field is also required, and a value is automatically generated for you when you create the taxon. Be careful with arbitrarily changing the permalink - if you have two taxons with the same permalink you will run into issues.
+*/
 
 export class Taxon {
   id: number;
