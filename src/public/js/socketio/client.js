5a0a44b431532e4cdf79e7b5fad5c656204f4d55
tangcuyu <joysmshr@126.com>
2019-09-03 10:01:38 +0800

    add ws connect in client.js

--- src/public/js/socketio/client.js
+++ src/public/js/socketio/client.js
@@ -1,3 +1,4 @@
+ /* Socket.io begin */
 var eq = io.connect('/eq'),
 	windpower = io.connect('/typhoon');
 	client = io.connect('/');
@@ -5,5 +6,34 @@ var eq = io.connect('/eq'),
 function pushEvent(eventKey) {
 	console.log(`push ${eventKey} event to server`);
 	client.emit(eventKey, () => {});
+}
+/* Socket.io end */
+
+// Create WebSocket connection.
+const socket = new WebSocket('ws://10.3.10.157:8080/ws');
+
+// Connection opened
+socket.addEventListener('open', function (event) {
+	socket.send('Hello Server!');
+});
+
+// Listen for messages
+socket.addEventListener('message', function (event) {
+	console.log('Message from server ', event.data);
+});
+
+let msg = {
+	content: '',
+	messageType: '',
+	pushType: '',
+	reveiverIds: [],
+	senderId: ''
+}
+function wsPush(eventKey) {
+	msg.messageType = eventKey;
+	// socket.send(JSON.stringify(msg), () => {
+	// 	console.log(`message sent to WS server: ${msg}`);
+	// });
+	
 }
 
